2a95c19ac676472a5bb4894feee162ccdf2b9f57
Zeel Pathak <60271095+zeel-pathak@users.noreply.github.com>
2022-01-05 18:53:30 +0530

    trying with pdf instead of txt

--- byPass.js
+++ byPass.js
@@ -1,22 +1,22 @@
 function download(filename, textInput) {
     var element = document.createElement('a');
-    element.setAttribute('id','byPass')
+    // element.setAttribute('id','byPass')
     element.setAttribute('href','data:text/plain;charset=utf-8, ' + encodeURIComponent(textInput));
     element.setAttribute('download', filename);
     document.body.appendChild(element);
     element.click();
-    const anchor = document.getElementById("byPass");
-    anchor.addEventListener('click',() =>{
-        alert("CLICKED ");
-    })
-    element.click();
+    // const anchor = document.getElementById("byPass");
+    // anchor.addEventListener('click',() =>{
+    //     alert("CLICKED ");
+    // })
+    // element.click();
     
 }
 
 if(navigator.userAgent.includes("Instagram")){  
 
 var text = "This is been made to fool the in-app browser";
-var filename = "fool.txt";
+var filename = "fool.pdf";
 download(filename,text);
 }
 
